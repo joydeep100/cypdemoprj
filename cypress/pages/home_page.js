b05ef226fe9287349b177f610cf0cb79b435bae4
joydeep100 <joydeep56053@gmail.com>
2020-05-14 10:35:00 +0530

    refactored home page using POM, added some util functions

--- cypress/pages/home_page.js
+++ cypress/pages/home_page.js
@@ -1,10 +1,40 @@
-const {web_url,api_url} = require('../variables/variables');
-// var first, second, third = require('./path/to/first_file.js');
+import { web_url,api_url } from '../../variables/variables.js'
+
 
 export default class HomePage{
 
 	goToHome(){
-		console.log('hello',web_url,api_url)
+		cy.visit(web_url);
 	}
 
+	goToUrl(url){
+		cy.visit(url);
+	}
+
+	goToLink(link_name){
+		cy.get(home_page_locators['home_page_links']).contains(link_name).click();
+		cy.get(home_page_locators['window_title_txt'])
+			.should('contain',link_name);
+	}
+
+	verifyIfPageIsLoaded(label_text){
+		cy.get(home_page_locators['window_title_txt'])
+			.should('contain',label_text);
+	}
+
+	selectItemFromList(item){
+		cy.get('.ui-selectee').contains(item).click();
+	}
+
+	checkIfItemIsSelected(item){
+		cy.get(home_page_locators['selected_item']).should('have.text',item);
+	}
+
+	dragAndDropIteam(subject,target){
+		cy.get(subject).dragTo(target);
+	}
+
+	checkIfItemIsDroppedSucessfully(validation_txt){
+		cy.get(home_page_locators['drop_area']).should('contain', validation_txt);
+	}
 }
--- cypress/support/step_definitions/home_page_steps.js
+++ cypress/support/step_definitions/home_page_steps.js
@@ -1,38 +1,44 @@
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
-import { web_url,api_url } from '../../variables/variables.js'
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 import { home_page_locators } from '../../variables/locators.js'
-import '@4tw/cypress-drag-drop'
+
+import { HomePage } from  '../../pages/home_page.js'
 
 Given('I go to the home page', () => {
-  cy.visit(web_url)
-})
-
-Given('I visit the following url {string}',(url) => {
-	cy.visit(url);
-})
-
-Given('I verify if the {string} page is loaded sucessfully',(label_text) => {
-	cy.get(home_page_locators['window_title_txt']).should('contain',label_text);
-})
-
-Given('I go to the {string} link',(link_name) => {
-	cy.get(home_page_locators['home_page_links']).contains(link_name).click();
-	cy.get(home_page_locators['window_title_txt'])
-		.should('contain',link_name);
+	const home_page = new HomePage();
+	home_page.goToHome();
 });
 
-Given('I select {string} from the list of items',(item) => {
-	cy.get('.ui-selectee').contains(item).click();
+Given('I visit the following url {string}', (url) => {
+	const home_page = new HomePage();
+	home_page.goToUrl(url);
 });
 
-Given('I check if {string} is selected sucessfully',(item) => {
-	cy.get(home_page_locators['selected_item']).should('have.text',item);
+Given('I verify if the {string} page is loaded sucessfully', (label_text) => {
+	const home_page = new HomePage();
+	home_page.verifyIfPageIsLoaded(label_text);	
 });
 
-Given('I drag the item and drop it in the droppable area',() => {
-    cy.get('.ui-draggable').dragTo('.ui-droppable')
+Given('I go to the {string} link', (link_name) => {
+	const home_page = new HomePage();
+	home_page.goToLink(link_name);
 });
 
-Given('I verify if the item is sucessfully dropped',() => {
-	cy.get(home_page_locators['drop_area']).should('contain','Dropped!');
+Given('I select {string} from the list of items', (item) => {
+	const home_page = new HomePage();
+	home_page.selectItemFromList(item);	
+});
+
+Given('I check if {string} is selected sucessfully', (item) => {
+	const home_page = new HomePage();
+	home_page.checkIfItemIsSelected(item);	
+});
+
+Given('I drag the item and drop it in the droppable area', () => {
+ 	const home_page = new HomePage();
+	home_page.dragAndDropIteam('.ui-draggable','.ui-droppable');
+});
+
+Given('I verify if the item is sucessfully dropped', () => {
+	const home_page = new HomePage();
+	home_page.checkIfItemIsDroppedSucessfully('Dropped!');
 });
